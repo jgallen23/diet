d57afe1a0649d193c8e650aeda3b8f9c14225ee0
Greg Allen <me@jga.me>
2011-11-13 12:20:44 -0800

    v0.0.1 - initial commit

---
+++ example/server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var diet = require('../');
+var app = express.createServer();
+
+diet.use(app, {
+  profiles: {
+    'test1': {
+      width: 150,
+      height: "50^",
+      customArgs: [
+        '-gravity', 'center',
+        '-extent', '150x50'
+      ]
+    }
+  },
+  profilesConfig: 'imgdiet.json'
+});
+
+app.set("view options", {
+  layout: false 
+});
+app.set("view engine", "jade");
+app.set("views", "" + __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  res.render('index', {
+  });
+});
+
+app.listen(3000);
