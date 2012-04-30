b88ffe6de02e2ca5737123d52d4ff0667beee250
Greg Allen <me@jga.me>
2012-04-30 14:32:48 -0700

    added bin to start diet server

---
+++ bin/diet.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var fs = require('fs');
+var Diet = require('../');
+
+
+var list = function(val) {
+  return val.split(',');
+};
+
+program
+  .version(JSON.parse(fs.readFileSync(__dirname + '/../package.json', 'utf8')).version)
+  .option('-s, --server <port>', 'Start web server on <port>')
+  .usage('[options] <config file>')
+  .parse(process.argv);
+
+
+var usage = function() {
+  process.stdout.write(program.helpInformation());
+  program.emit('--help');
+  process.exit(1);
+};
+
+if (program.args.length !== 0) {
+
+  if (program.server) {
+    var config = program.args[0];
+
+    Diet.server(config, program.server);
+  } else {
+    usage();
+  }
+} else {
+  usage();
+}
---
+++ examples/bin/README.md
@@ -0,0 +1,7 @@
+Run this:
+
+	node ../../bin/diet.js -s 8001 diet.json
+
+Then go here:
+
+	http://localhost:8001/thumbnail/steve-jobs.jpg
